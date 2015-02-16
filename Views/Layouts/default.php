6cd43779a1201014dbd24787e3eff2bb999aeb51
Mathieu <m.boisnard@free.fr>
2015-02-16 23:28:28 +0100

    Favicon

--- Views/Layouts/default.php
+++ Views/Layouts/default.php
@@ -4,6 +4,7 @@
 	<title><?php echo ($title_for_layout) ? $title_for_layout : 'Page'; ?></title>
 	<?php echo $this->Html->charset(); ?>
 	<?php echo $this->Html->meta('viewport', 'width=device-width, initial-scale=1.0'); ?>
+	<?php echo $this->Html->meta('icon', 'img/favicon.ico'); ?>
 
 	<?php echo $this->Html->layout('meta'); ?>
 </head>
