b82e3826e8c70fc6683b076c12f65833cb9c382c
Mathieu <m.boisnard@free.fr>
2015-02-18 14:58:06 +0100

    JQuery

--- Views/Cloud/index.php
+++ Views/Cloud/index.php
@@ -1,5 +1,27 @@
+<?php header("Access-Control-Allow-Origin: *"); ?>
 <?php $title_for_layout = 'Cloud | ENK-Cloud'; ?>
+<?php echo $this->Html->script('cloud'); ?>
+
 <h1>Page de Cloud</h1>
 <?php echo var_dump($this->Session->read()); ?>
 <br/>
+<br/>
+<div id="client_files"></div>
+<div id="dev_files"></div>
+<br/>
 <?php echo $this->Html->link('Logout', array('controller' => 'users', 'action' => 'logout')); ?>
+
+<?php 
+	echo $this->Form->input('Token.link', array(
+		'type' 	=> 'hidden',
+		'id'	=> 'link',
+		'value' => $this->Session->read('Token.link')
+	));
+?>
+<?php 
+	echo $this->Form->input('Token.fields', array(
+		'type' 	=> 'hidden',
+		'id'	=> 'fields',
+		'value' => $this->Session->read('Token.fields')
+	));
+?>
