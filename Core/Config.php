87db885f6697922775fda18c95f6ce06dee7a556
MrKloan <contact@fries.io>
2015-03-16 10:11:39 +0100

    Coucou

--- Core/Config.php
+++ Core/Config.php
@@ -31,8 +31,8 @@ define('IMG_ROOT', ROOT.DS.'webroot'.DS.'img'.DS);
 define('FONTS_ROOT', ROOT.DS.'webroot'.DS.'fonts'.DS);
 define('FILES_ROOT', ROOT.DS.'webroot'.DS.'files'.DS);
 
-define('INI_FILES', DS.'Users'.DS.'Worker'.DS.'Desktop'.DS.'ini'.DS);
-//define('INI_FILES', '/var/www/ini/');
+//define('INI_FILES', DS.'Users'.DS.'Worker'.DS.'Desktop'.DS.'ini'.DS);
+define('INI_FILES', '/var/www/ini/');
 
 // Database
 class DBConfig
