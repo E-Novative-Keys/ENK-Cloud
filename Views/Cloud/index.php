5fb57ba75a8f2768176c3ee0c05b8828cf31a82e
Mathieu <m.boisnard@free.fr>
2015-04-28 11:08:30 +0200

    Suppression des prompts

--- Views/Cloud/index.php
+++ Views/Cloud/index.php
@@ -63,4 +63,31 @@
             </div>
         </div>  
     </div>
+
+    <div class="modal fade" id="renameModal" tabindex="-1" role="dialog" aria-labelledby="rename" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Fermer"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="rename">Renommer un fichier/dossier</h4>
+                </div>
+            
+                <form class="form-horizontal">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="dir" class="col-sm-3 control-label">Nouveau nom</label>
+                            <div class="col-sm-8">
+                                <input type="text" id="renameField" placeholder="Nouveau nom" class="form-control" required />
+                            </div>
+                        </div>
+                    </div>
+                        
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+                        <button type="button" id="renameSubmit" class="btn btn-primary" data-dismiss="modal">Créer</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </section>
--- webroot/js/filesManager.js
+++ webroot/js/filesManager.js
@@ -97,7 +97,7 @@ function listFiles(user, dir, search)
 */
 function folderCreate() 
 {
-    var name = prompt("Nouveau dossier", "");
+    var name = $('#dir').val();
 
     var dir = $('#storage').attr("data-dir");
 
@@ -123,48 +123,53 @@ function folderCreate()
 */
 function renameFile(tr)
 {
-    var rename = prompt("New file name", "");
+    $('#renameModal').modal('show');
 
-    var dir = atob(tr.attr("data-file"));
-    var args = dir.split('/');
-    var name = '';
+    $('#renameSubmit').on('click', function(){
+        var rename = $('#renameField').val();
 
-    if(tr.attr("data-dir") == "false")
-    {
-        name = args[args.length-1];
-        dir = dir.replace(args[args.length-1], '');
-    }
+        var dir     = atob(tr.attr("data-file"));
+        var args    = dir.split('/');
+        var name    = '';
 
-    if(rename)
-    {
-        $.ajax({
-            type : "POST",
-            url : "http://enkwebservice.com/cloud/files/rename",
-            data : "data=" + JSON.stringify({data : {
-                Cloud : {project : $('.projects-button').attr("data-project"), directory : btoa(dir), name : name, rename : rename},
-                Token : {link : $('#link').val(), fields : $('#fields').val()}
-            }}),
-            dataType : "json",
-            crossDomain : true
-        })
-        .success(function(data) {
-            args[args.length-1] = rename;
-            tr.find('td:first-child span').text(rename);
-            tr.attr("data-file", btoa(args.join('/')));
+        if(tr.attr("data-dir") == "false")
+        {
+            name = args[args.length-1];
+            dir = dir.replace(args[args.length-1], '');
+        }
 
-            var ext = ((args = rename.split('.')) != undefined && args.length > 1) ? args.pop() : '';
+        if(rename)
+        {
+            $.ajax({
+                type : "POST",
+                url : "http://enkwebservice.com/cloud/files/rename",
+                data : "data=" + JSON.stringify({data : {
+                    Cloud : {project : $('.projects-button').attr("data-project"), directory : btoa(dir), name : name, rename : rename},
+                    Token : {link : $('#link').val(), fields : $('#fields').val()}
+                }}),
+                dataType : "json",
+                crossDomain : true
+            })
+            .success(function(data) {
+                args[args.length-1] = rename;
+                tr.find('td:first-child span').text(rename);
+                tr.attr("data-file", btoa(args.join('/')));
 
-            if(tr.attr("data-dir") == "true")
-                tr.find('td:first-child img').attr("src", "img/icons/directory.png");
-            else if(ext.length > 0)
-                tr.find('td:first-child img').attr("src", "img/icons/" + ext + ".png");
+                var ext = ((args = rename.split('.')) != undefined && args.length > 1) ? args.pop() : '';
 
-            tr.find('td:nth-child(3)').html(ext);
-        })
-        .fail(function(e){
-            alert(e);
-        });
-    }
+                if(tr.attr("data-dir") == "true")
+                    tr.find('td:first-child img').attr("src", "img/icons/directory.png");
+                else if(ext.length > 0)
+                    tr.find('td:first-child img').attr("src", "img/icons/" + ext + ".png");
+
+                tr.find('td:nth-child(3)').html(ext);
+            })
+            .fail(function(e){
+                alert(e);
+            });
+        }
+    });
+    
 }
 
 /**
